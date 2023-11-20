7652782abd432d1f3052f51a60a54c4e1d471b33
OgataiKhan <badellino.kevin@gmail.com>
2023-11-20 16:18:01 +0100

    feat: make objects array

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const members = [
+    {
+        name: "Wayne Barnett",
+        position: "Founder & CEO",
+        photo: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        position: "Chief Editor",
+        photo: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        position: "Office Manager",
+        photo: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        position: "Social Media Manager",
+        photo: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        position: "Developer",
+        photo: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        position: "Graphic Designer",
+        photo: "barbara-ramos-graphic-designer.jpg"
+    }
+];
